b4a802870fc4a60f61b0cbe8a97ee61a0916fb4b
patryk-penczek <desparvek@gmail.com>
2025-02-18 16:47:15 +0100

    feat: project init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'CrustLab Finance Panel',
+  description:
+    'Admin panel for managing user accounts and financial transactions, built with Next.js and TypeScript.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
